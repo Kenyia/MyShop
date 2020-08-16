5fac6a7933ab7955f837ee16ccba4efd2fb88e7e
Kenyia L Salter <kenyiasalter@gmail.com>
2020-08-15 22:09:29 -0500

    Update Product.cs

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// I would change the variables and product Category because it's confusing because you have it more than one class. I think they are the same objects so you could make one class the base class
+//(Parent) and the other class a child the child can use fields (objects) from parent but parent can't take from child.
+
+
 namespace MyShop.Core.Models
 {
     public class Product : BaseEntity
